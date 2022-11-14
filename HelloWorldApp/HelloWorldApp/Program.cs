afddc37d3aaa9f362f092d3f3ce1ab9903cd8d94
mdahirr <m.dahir@hotmail.co.uk>
2022-11-14 15:24:51 +0000

    add code up to command line args demo

--- HelloWorldApp/HelloWorldApp/Program.cs
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorldApp
+
+{
+    public class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            int x = 100;
+            x += 10;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("i: " + i + " -- x: " + x);
+
+                x += 1;
+            }
+
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
