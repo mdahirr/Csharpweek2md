1992c1c52803da9f9f6c1b738376d81d4f1b8085
mdahirr <m.dahir@hotmail.co.uk>
2022-11-16 11:14:50 +0000

    add increment and decrement examples

---
+++ OperatorsApp/OperatorsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿int x = 5;
+int y = 5;
+
+int postfix = x++;
+int prefix = ++y;
+
+Console.WriteLine($"postfix: {postfix}, prefix {prefix}");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i);
+}
