ef9193e9b37776892316caa574f04d5d882e61f4
mdahirr <m.dahir@hotmail.co.uk>
2022-11-16 11:28:27 +0000

    finish integar division section

--- OperatorsApp/OperatorsApp/Program.cs
+++ OperatorsApp/OperatorsApp/Program.cs
@@ -1,12 +1,28 @@
-﻿int x = 5;
-int y = 5;
+﻿#region postfix and prefix
 
-int postfix = x++;
-int prefix = ++y;
+//int x = 5;
+//int y = 5;
 
-Console.WriteLine($"postfix: {postfix}, prefix {prefix}");
+//int postfix = x++;
+//int prefix = ++y;
 
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine(i);
-}
+//Console.WriteLine($"postfix: {postfix}, prefix {prefix}");
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine(i);
+//}
+#endregion
+#region integar division
+//var c = 5 / 2;    // two integers will result in an integer, the remainder is ignored
+//var d = 5.0 / 2;  // where one is a double, the other will be converted to a double
+//var e = 5 / 3;
+
+
+
+//Console.WriteLine($"c == {c} : d == {d} : e == {e}");
+
+//double f = 5 / 2;
+
+//Console.WriteLine(f);
+#endregion
