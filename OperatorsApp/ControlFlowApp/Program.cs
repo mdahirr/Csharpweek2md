1986403401f1bd73ecf0691d3e0ec6fda800cffd
mdahirr <m.dahir@hotmail.co.uk>
2022-11-16 14:47:28 +0000

    control flow test attempted

---
+++ OperatorsApp/ControlFlowApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//int mark = 35;
+//var grade = mark >= 65 ? (mark >= 85 ? "Distinction" : "Pass") : "Fail";
+
+namespace grades
+{
+    public class GradeMark
+    {
+        public string GradeMethod(int mark)
+        {
+            var grade = mark >= 65 ? (mark >= 85 ? "Distinction" : "Pass") : "Fail";
+            return grade;
+        }
+    }
+}
