dc763e8ffc9a59e398c91eff112841916f4b3070
mdahirr <m.dahir@hotmail.co.uk>
2022-11-16 15:21:56 +0000

    added switch statements

--- OperatorsApp/ControlFlowApp/Program.cs
+++ OperatorsApp/ControlFlowApp/Program.cs
@@ -1,9 +1,13 @@
 ﻿//int mark = 35;
 //var grade = mark >= 65 ? (mark >= 85 ? "Distinction" : "Pass") : "Fail";
 
-namespace grades
+string greeting = null;
+if (greeting != null & greeting.ToLower().StartsWith('a'))
 {
-    public class GradeMark
+    Console.WriteLine($"{greeting} starts with 'a'");
+}
+
+public class GradeMark
     {
         public string GradeMethod(int mark)
         {
@@ -11,4 +15,13 @@ namespace grades
             return grade;
         }
     }
+
+public partial class Program
+{
+    public static string Grade(int mark)
+    {
+        var grade = mark >= 65 ? (mark >= 85 ? "Distinction" : "Pass") : "Fail";
+        return grade;
+    }
 }
+
--- OperatorsApp/OperatorsControlFlow.Tests/ControlFlowTests.cs
+++ OperatorsApp/OperatorsControlFlow.Tests/ControlFlowTests.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace OperatorsControlFlow.Tests
 {
-    internal class ControlFlowTests
+    public class ControlFlowTests
     {
+        [TestCase(90, "Pass")]
+
+        public void GradeMarkTest(int mark)
+        {
+            string expectedResult = "Pass";
+
+            string result = OperatorsApp.Program.GradeMethod(mark);
+
+            Assert.That(expectedResult, Is.Equalto(result));
+
+        }
     }
 }
