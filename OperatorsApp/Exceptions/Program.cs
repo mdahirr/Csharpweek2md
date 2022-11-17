6f90c4b6d5dc0875064bd0dc4ef1f34068f76088
mdahirr <m.dahir@hotmail.co.uk>
2022-11-17 12:02:25 +0000

    added exception types to finally block

---
+++ OperatorsApp/Exceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            string fileName = "";
+
+            try
+            {
+                text = File.ReadAllText(fileName);
+                Console.WriteLine(text);
+            }
+            
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"Sorry {fileName} could not be found");
+            }
+            catch (ArgumentException e) 
+            {
+                Console.WriteLine("An empty filename was supplied");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Generally handled");
+            }
+            finally
+            {
+                Console.WriteLine("We made it here!");
+            }
+        }
+    }
+}
