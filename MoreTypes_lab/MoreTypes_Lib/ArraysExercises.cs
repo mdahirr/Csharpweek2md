2b6b023b79bf4a5cb7a0d58034edc878721a82dc
mdahirr <m.dahir@hotmail.co.uk>
2022-11-18 16:28:50 +0000

    moreTypesLabs string exercies complete

---
+++ MoreTypes_lab/MoreTypes_Lib/ArraysExercises.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoreTypes_Lib
+{
+    public class ArraysExercises
+    {
+        // returns a 1D array containing the contents of a given List
+        public static string[] Make1DArray(List<string> contents)
+        {
+            throw new NotImplementedException();
+        }
+
+        // returns a 3D array containing the contents of a given List
+        public static string[,,] Make3DArray(int length1, int length2, int length3, List<string> contents)
+        {
+            throw new NotImplementedException();
+        }
+
+        // returns a jagged array containing the contents of a given List
+        public static string[][] MakeJagged2DArray(int countRow1, int countRow2, List<string> contents)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MoreTypes_lab/MoreTypes_Lib/StringExercises.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace MoreTypes_Lib
+{
+    public class StringExercises
+    {
+        // manipulates and returns a string - see the unit test for requirements
+        public static string ManipulateString(string input, int num)
+        {
+            string firstName = input.ToUpper().Trim();
+
+            string totalNums = "";
+            for (int i = 0; i < num; i++)
+            {   
+                
+                totalNums +=$"{i}";
+            }
+
+            return firstName + totalNums;
+            
+            throw new NotImplementedException();
+        }
+
+        // returns a formatted address string given its components
+        public static string Address(int number, string street, string city, string postcode)
+        {
+            string doorNum = number.ToString();
+            string FullAddress = $"{doorNum} {street}, {city} {postcode}.";
+            return FullAddress;
+            throw new NotImplementedException();
+        }
+        // returns a string representing a test score, written as percentage to 1 decimal place
+        public static string Scorer(int score, int outOf)
+        {
+            double finalScore = ((double) score / (double) outOf) * 100;
+            double finalScoreRnded = Math.Round(finalScore, 1);
+            string result = $"You got {score} out of {outOf}: {finalScoreRnded}%";
+            return result;
+            throw new NotImplementedException();
+        }
+
+        // returns the double represented by the string, or -999 if conversion is not possible
+        public static double ParseNum(string numString)
+        {
+            double result;
+            bool success = double.TryParse(numString, out result);
+            if (success)
+            {
+                return result;
+            }
+            return - 999;
+            throw new NotImplementedException();
+        }
+
+        // Returns the a string containing the count of As, Bs, Cs and Ds in the parameter string
+        // all other letters are ignored
+        public static string CountLetters(string input)
+        {
+            int ACount = 0;
+            int BCount = 0;
+            int CCount = 0;
+            int DCount = 0;
+
+            foreach (char letter in input)
+            {
+                if (letter == 'A') 
+                {
+                    ACount++;
+                }
+                else if (letter == 'B')
+                {
+                    BCount++;
+                }
+                else if (letter == 'C')
+                {
+                    CCount++;
+                }
+                else if (letter == 'D')
+                {
+                    DCount++;
+                }
+            }
+
+            return $"A:{ACount} B:{BCount} C:{CCount} D:{DCount}";
+
+            throw new NotImplementedException();
+        }
+    }
+}
