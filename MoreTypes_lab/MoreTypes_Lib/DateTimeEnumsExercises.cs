d737bc1ef6ac42037d9c2e24a25951b924b65ff9
mdahirr <m.dahir@hotmail.co.uk>
2022-11-20 19:07:40 +0000

    Enum exercise added

--- MoreTypes_lab/MoreTypes_Lib/DateTimeEnumsExercises.cs
+++ MoreTypes_lab/MoreTypes_Lib/DateTimeEnumsExercises.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http.Headers;
 
 namespace MoreTypes_Lib
 {
@@ -11,24 +12,53 @@ namespace MoreTypes_Lib
         // returns a person's age at a given date, given their birth date.
         public static int AgeAt(DateTime birthDate, DateTime date)
         {
-            return -1;
+            var month = date.Month - birthDate.Month;
+            var age = date.Year - birthDate.Year;
+            if (month < 0)
+            {
+                age -= 1;
+            }
+            if (birthDate > date)
+            {
+                throw new ArgumentException("Error - birthDate is in the future");
+            }
+            return age;
         }
         // returns a date formatted in the manner specified by the unit test
         public static string FormatDate(DateTime date)
         {
-            return string.Empty;
+            string newdate = date.ToString("%y/%d/MMM");
+            return newdate;
         }
 
         // returns the name of the month corresponding to a given date
         public static string GetMonthString(DateTime date)
         {
-            return string.Empty;
+            string month = date.ToString("MMMM");
+            return month;
         }
 
         // see unit tests for requirements
         public static string Fortune(Suit suit)
         {
-            return string.Empty;
+            if (suit == Suit.CLUBS)
+            {
+                return "And the seventh rule is if this is your first night at fight club, you have to fight.";
+            }
+            else if (suit == Suit.SPADES)
+            {
+                return "Bucket and spade";
+            }
+            else if (suit == Suit.DIAMONDS)
+            {
+                return "Diamonds are a girls best friend";
+            }
+            else if (suit == Suit.HEARTS)
+            {
+                return "You've broken my heart";
+            }
+
+            return "nothing";
         }
     }
 }
