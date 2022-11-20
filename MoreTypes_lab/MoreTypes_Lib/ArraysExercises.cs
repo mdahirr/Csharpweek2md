f0754a4c52d103733452fb053ed3971ba9dc1952
mdahirr <m.dahir@hotmail.co.uk>
2022-11-20 21:22:20 +0000

    array exercies attempted

--- MoreTypes_lab/MoreTypes_Lib/ArraysExercises.cs
+++ MoreTypes_lab/MoreTypes_Lib/ArraysExercises.cs
@@ -8,13 +8,24 @@ namespace MoreTypes_Lib
         // returns a 1D array containing the contents of a given List
         public static string[] Make1DArray(List<string> contents)
         {
-            throw new NotImplementedException();
+            return contents.ToArray();
         }
 
         // returns a 3D array containing the contents of a given List
         public static string[,,] Make3DArray(int length1, int length2, int length3, List<string> contents)
         {
-            throw new NotImplementedException();
+            string[,,] threeDList = new string[length1, length2, length3];
+
+            for (int x = 0; x < length1; x++)
+            {
+                for (int y = 0; y < length2; y++)
+                {
+                    for (int z = 0; z < length3; z++)
+                    {
+                        string threeDList[x][y][z] = contents.ToArray();
+                    }
+                }
+            }
         }
 
         // returns a jagged array containing the contents of a given List
