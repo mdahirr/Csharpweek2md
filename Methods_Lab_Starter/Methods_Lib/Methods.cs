2d32eced00bea9bce556821f3793f39a4d1e4458
mdahirr <m.dahir@hotmail.co.uk>
2022-11-21 16:35:56 +0000

    method lab exercies

---
+++ Methods_Lab_Starter/Methods_Lib/Methods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Methods_Lib
+{
+    public class Methods
+    {
+        // implement this method so it returns a tuple (weeks, days) 
+        // corresponding to a given number of days
+        public static (int weeks, int days) DaysAndWeeks(int totalDays)
+        {
+            if (totalDays < 0)
+            {
+                throw new ArgumentOutOfRangeException("totalDays must not be negative");
+            }
+                int weeks = totalDays / 7;
+                int days = totalDays % 7;
+
+                return (weeks, days);
+            }
+            
+            //throw new NotImplementedException();
+
+        }
+
+        static int RollDice(Random rng)
+        {
+            int num1 = rng.Next(1, 7);
+            int num2 = rng.Next(1, 7);
+            return num1 + num2;
+        }
+
+        public static (int num1, num2) PowerRoot(int num1)
+        {
+            int squared = num1 * num1;
+            int cubed = num1* num1* num1;
+            
+        }
+
+    }
+}
